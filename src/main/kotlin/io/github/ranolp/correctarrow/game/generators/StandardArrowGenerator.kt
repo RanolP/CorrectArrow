4ae6ea73425d0b511283eeac8c0c509d731eb236
Ranol <asdhugh1@gmail.com>
2017-09-18 21:32:59 +0900

    Implementing standard generator

---
+++ src/main/kotlin/io/github/ranolp/correctarrow/game/generators/StandardArrowGenerator.kt
@@ -0,0 +1,25 @@
+package io.github.ranolp.correctarrow.game.generators
+
+import io.github.ranolp.correctarrow.game.Arrow
+import io.github.ranolp.correctarrow.game.ArrowFrom
+import io.github.ranolp.correctarrow.game.ArrowGenerator
+import io.github.ranolp.correctarrow.game.ArrowTo
+
+class StandardArrowGenerator(vararg allowedDirections: ArrowFrom) : ArrowGenerator {
+    // for remove duplicated directions.
+    private val allowed = setOf(*allowedDirections)
+
+    override fun generate(from: ArrowFrom, size: Int): List<Arrow> {
+        return if (from !in allowed) {
+            emptyList()
+        } else {
+            val generator = when (from) {
+                ArrowFrom.LEFT -> ArrowTo.Companion::randomRight
+                ArrowFrom.RIGHT -> ArrowTo.Companion::randomLeft
+                ArrowFrom.TOP -> ArrowTo.Companion::randomBottom
+                ArrowFrom.BOTTOM -> ArrowTo.Companion::randomTop
+            }
+            List(size, { Arrow(from, generator()) })
+        }
+    }
+}
