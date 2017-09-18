969911ed3118e87313522001123413cda9791e90
Ranol <asdhugh1@gmail.com>
2017-09-18 21:08:10 +0900

    Add enums

---
+++ src/main/kotlin/io/github/ranolp/correctarrow/game/ArrowFrom.kt
@@ -0,0 +1,8 @@
+package io.github.ranolp.correctarrow.game
+
+enum class ArrowFrom {
+    LEFT,
+    RIGHT,
+    TOP,
+    BOTTOM
+}
---
+++ src/main/kotlin/io/github/ranolp/correctarrow/game/ArrowTo.kt
@@ -0,0 +1,12 @@
+package io.github.ranolp.correctarrow.game
+
+enum class ArrowTo {
+    LEFT,
+    RIGHT,
+    TOP,
+    BOTTOM,
+    TOP_LEFT,
+    BOTTOM_LEFT,
+    TOP_RIGHT,
+    BOTTOM_RIGHT
+}
