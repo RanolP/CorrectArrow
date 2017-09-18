4c83d360096e777a9876c103ade00fbef22f2587
Ranol <asdhugh1@gmail.com>
2017-09-18 21:45:10 +0900

    Remove private accessor in fields

--- src/main/kotlin/io/github/ranolp/correctarrow/game/Board.kt
+++ src/main/kotlin/io/github/ranolp/correctarrow/game/Board.kt
@@ -3,8 +3,8 @@ package io.github.ranolp.correctarrow.game
 import io.github.ranolp.correctarrow.game.generators.StandardArrowGenerator
 
 class Board(val height: Int, val width: Int, val generator: ArrowGenerator = StandardArrowGenerator.ALLOW_ALL) {
-    private val leftArrows: List<Arrow> = generator.generate(ArrowFrom.LEFT, height)
-    private val rightArrows: List<Arrow> = generator.generate(ArrowFrom.RIGHT, height)
-    private val topArrows: List<Arrow> = generator.generate(ArrowFrom.TOP, width)
-    private val bottomArrows: List<Arrow> = generator.generate(ArrowFrom.BOTTOM, width)
+    val leftArrows: List<Arrow> = generator.generate(ArrowFrom.LEFT, height)
+    val rightArrows: List<Arrow> = generator.generate(ArrowFrom.RIGHT, height)
+    val topArrows: List<Arrow> = generator.generate(ArrowFrom.TOP, width)
+    val bottomArrows: List<Arrow> = generator.generate(ArrowFrom.BOTTOM, width)
 }
