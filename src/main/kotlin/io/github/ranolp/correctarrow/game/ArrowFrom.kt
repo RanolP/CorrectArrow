c7f969c4ece3a2044352dd3e673f25b947c4d7a9
Ranol <asdhugh1@gmail.com>
2017-09-18 21:16:04 +0900

    Add id of direction

--- src/main/kotlin/io/github/ranolp/correctarrow/game/ArrowFrom.kt
+++ src/main/kotlin/io/github/ranolp/correctarrow/game/ArrowFrom.kt
@@ -1,8 +1,8 @@
 package io.github.ranolp.correctarrow.game
 
-enum class ArrowFrom {
-    LEFT,
-    RIGHT,
-    TOP,
-    BOTTOM
+enum class ArrowFrom(val id: Int) {
+    LEFT(0),
+    RIGHT(1),
+    TOP(2),
+    BOTTOM(3)
 }
--- src/main/kotlin/io/github/ranolp/correctarrow/game/ArrowTo.kt
+++ src/main/kotlin/io/github/ranolp/correctarrow/game/ArrowTo.kt
@@ -1,12 +1,12 @@
 package io.github.ranolp.correctarrow.game
 
-enum class ArrowTo {
-    LEFT,
-    RIGHT,
-    TOP,
-    BOTTOM,
-    TOP_LEFT,
-    BOTTOM_LEFT,
-    TOP_RIGHT,
-    BOTTOM_RIGHT
+enum class ArrowTo(vararg val id: Int) {
+    LEFT(0),
+    RIGHT(1),
+    TOP(2),
+    BOTTOM(3),
+    TOP_LEFT(0, 2),
+    BOTTOM_LEFT(0, 3),
+    TOP_RIGHT(1, 2),
+    BOTTOM_RIGHT(1, 3)
 }
