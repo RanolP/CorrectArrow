8f91c4e01e05984f0480af48c89d8faba84dcb0f
Ranol <asdhugh1@gmail.com>
2017-09-18 21:46:03 +0900

    Convert arrow to data class

--- src/main/kotlin/io/github/ranolp/correctarrow/game/Arrow.kt
+++ src/main/kotlin/io/github/ranolp/correctarrow/game/Arrow.kt
@@ -2,7 +2,7 @@ package io.github.ranolp.correctarrow.game
 
 import io.github.ranolp.correctarrow.error.SameDirectionError
 
-class Arrow(val from: ArrowFrom, val to: ArrowTo) {
+data class Arrow(val from: ArrowFrom, val to: ArrowTo) {
     init {
         if(from.id in to.id) {
             throw SameDirectionError(from, to)
