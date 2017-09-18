d9873daf7ea692f88dd6da71f6ec3d9c0986bec8
Ranol <asdhugh1@gmail.com>
2017-09-18 21:38:16 +0900

    Add arrow field in board

--- src/main/kotlin/io/github/ranolp/correctarrow/game/Board.kt
+++ src/main/kotlin/io/github/ranolp/correctarrow/game/Board.kt
@@ -1,5 +1,8 @@
 package io.github.ranolp.correctarrow.game
 
 class Board(val height: Int, val width: Int, val generator: ArrowGenerator) {
-
+    private val leftArrows: List<Arrow> = generator.generate(ArrowFrom.LEFT, height)
+    private val rightArrows: List<Arrow> = generator.generate(ArrowFrom.RIGHT, height)
+    private val topArrows: List<Arrow> = generator.generate(ArrowFrom.TOP, width)
+    private val bottomArrows: List<Arrow> = generator.generate(ArrowFrom.BOTTOM, width)
 }
